3bfc9bbb96865eb6360562436acad5a31171ffba
Justin Huang <jstn@cs.washington.edu>
2017-08-17 18:09:18 -0700

    Added file to store constants.

---
+++ include/skin_segmentation/constants.h
@@ -0,0 +1,13 @@
+#ifndef _SKINSEG_CONSTANTS_H_
+#define _SKINSEG_CONSTANTS_H_
+
+namespace skinseg {
+static const char kRgbConfigPath[] = "/config/rgb_calibration.yml";
+static const char kThermalConfigPath[] = "/config/thermal_calibration.yml";
+static const char kRgbTopic[] = "/camera/rgb/image_rect_color";
+static const char kDepthTopic[] =
+    "/camera/depth_registered/hw_registered/image_rect";
+static const char kThermalTopic[] = "/ici/ir_camera/image_raw";
+}  // namespace skinseg
+
+#endif  // _SKINSEG_CONSTANTS_H_
