bc7da4bb9f90eb2d8cb57e3694ef0b8f88efb293
Justin Huang <jstn@cs.washington.edu>
2017-08-17 18:29:13 -0700

    Added normalized topic.

--- include/skin_segmentation/constants.h
+++ include/skin_segmentation/constants.h
@@ -8,6 +8,8 @@ static const char kRgbTopic[] = "/camera/rgb/image_rect_color";
 static const char kDepthTopic[] =
     "/camera/depth_registered/hw_registered/image_rect";
 static const char kThermalTopic[] = "/ici/ir_camera/image_raw";
+static const char kNormalizedThermalTopic[] =
+    "/ici/ir_camera/image_normalized_rgb";
 }  // namespace skinseg
 
 #endif  // _SKINSEG_CONSTANTS_H_
