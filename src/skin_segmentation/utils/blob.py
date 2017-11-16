605da44cb677ebd643639773e9fea2193b4d092d
Justin Huang <jstn@cs.washington.edu>
2017-11-16 14:09:19 -0800

    Removed unused function.

--- src/skin_segmentation/utils/blob.py
+++ src/skin_segmentation/utils/blob.py
@@ -40,28 +40,6 @@ def im_list_to_blob(ims, num_channels):
     return blob
 
 
-def prep_im_for_blob(im, pixel_means, target_size, max_size):
-    """Mean subtract and scale an image for use in a blob."""
-    im = im.astype(np.float32, copy=False)
-    im -= pixel_means
-    im_shape = im.shape
-    im_size_min = np.min(im_shape[0:2])
-    im_size_max = np.max(im_shape[0:2])
-    im_scale = float(target_size) / float(im_size_min)
-    # Prevent the biggest axis from being more than MAX_SIZE
-    if np.round(im_scale * im_size_max) > max_size:
-        im_scale = float(max_size) / float(im_size_max)
-    im = cv2.resize(
-        im,
-        None,
-        None,
-        fx=im_scale,
-        fy=im_scale,
-        interpolation=cv2.INTER_LINEAR)
-
-    return im, im_scale
-
-
 def pad_im(im, factor, value=0):
     """Pads the image so its width and height are a multiple of the given factor.
 
