d235f3aabc23d0abf61a26ab76efb48653ad666f
Justin Huang <jstn@cs.washington.edu>
2017-11-17 16:58:02 -0800

    Added script to run demos.

--- scripts/run_demo.bash
+++ scripts/run_demo.bash
@@ -4,3 +4,4 @@ source /home/jstn/.bashrc
 source /opt/ros/indigo/setup.bash
 source /home/jstn/tracking_ws/devel/setup.sh
 rosrun skin_segmentation hand_segmentation_demo.py /home/jstn/data/hand_models/2017-11-16_data24_100k/vgg16_fcn_rgbd_single_frame_hand_iter_100000.ckpt rgb:=/camera/rgb/image_rect_color depth_registered:=/camera/depth_registered/image
+#rosrun skin_segmentation hand_segmentation_demo.py /home/jstn/data/hand_models/2017-11-17_data24_chromatic_100k/vgg16_fcn_rgbd_single_frame_hand_iter_100000.ckpt rgb:=/camera/rgb/image_rect_color depth_registered:=/camera/depth_registered/image
