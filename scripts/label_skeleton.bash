01add9a06988fc8606edca83aa686cfd0b3a6716
Justin Huang <jstn@cs.washington.edu>
2017-12-07 19:51:16 -0800

    Do not create folder, just create a file.

--- scripts/label_skeleton.bash
+++ scripts/label_skeleton.bash
@@ -8,6 +8,5 @@ fi
 source /opt/ros/indigo/setup.bash
 source $HOME/tracking_ws/devel/setup.bash
 
-mkdir -p $HOME/data/skel_labels/$1
-rm -r $HOME/data/skel_labels/$1/*
+rm $HOME/data/skel_labels/$1_labels.bag
 rosrun skin_segmentation label_skeleton $HOME/data/skels/$1.bag $HOME/data/skel_labels/$1_labels.bag
